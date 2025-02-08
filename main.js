750366f17c6751ee6cc407a2392689d3f0a15d02
Khaled Ahmed Swidan <37634845+KhaledSwidan@users.noreply.github.com>
2025-02-08 17:45:39 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-const elements = {
-  overlay: document.querySelector('.overlay'),
-  btn: document.querySelector('.mode-btn'),
-  h1: document.querySelector('h1'),
-  lastP: document.querySelector('.container p:last-of-type'),
-  jobTitle: document.querySelector('.container p span'),
-  projects: document.querySelector('.projects'),
-};
-
-const toggleElements = [elements.h1, elements.lastP, elements.projects];
-
-elements.btn.addEventListener('click', () => {
-  const isActive = elements.h1.classList.contains('active');
-
-  toggleElements.forEach((el) => el.classList.toggle('active'));
-  elements.jobTitle.style.color = isActive ? '#000' : '#fff';
-  elements.overlay.style.background = isActive ? '#e2e8f0' : '#1a202c';
-  elements.btn.innerHTML = isActive ? '🌙' : '☀️';
-});
-
 const techSkills = [
   'HTML5 - ',
   'CSS3 - ',
